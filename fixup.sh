cd50ed27abd7c4746c618ea13b1f14e8f9d54ac0
junderw <junderwood@bitcoinbank.co.jp>
2021-10-19 03:22:07 +0900

    Fix package.json to work better with webpack

--- fixup.sh
+++ fixup.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 cat >src/cjs/package.json <<!EOF
 {
-    "type": "commonjs"
+    "type": "commonjs",
+    "main": "index.js",
+    "browser": "browser.js"
 }
 !EOF
 
 cat >src/mjs/package.json <<!EOF
 {
-    "type": "module"
+    "type": "module",
+    "main": "index.js",
+    "browser": "browser.js"
 }
 !EOF
