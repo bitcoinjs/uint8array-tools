489ad4e3e8071eb6fc548f844a5ba0fc267d2cf3
ayman <aymanshaik1015@gmail.com>
2024-06-27 16:15:01 +0530

    chore: format

--- ts_src/browser.ts
+++ ts_src/browser.ts
@@ -1,18 +1,18 @@
 const HEX_STRINGS = "0123456789abcdefABCDEF";
 const HEX_CODES: number[] = HEX_STRINGS.split("").map((c) => c.codePointAt(0)!);
 const HEX_CODEPOINTS: (number | undefined)[] = Array(256)
-    .fill(true)
-    .map((_, i) => {
-        const s = String.fromCodePoint(i);
-        const index = HEX_STRINGS.indexOf(s);
-        // ABCDEF will use 10 - 15
-        return index < 0 ? undefined : index < 16 ? index : index - 6;
-    });
+  .fill(true)
+  .map((_, i) => {
+    const s = String.fromCodePoint(i);
+    const index = HEX_STRINGS.indexOf(s);
+    // ABCDEF will use 10 - 15
+    return index < 0 ? undefined : index < 16 ? index : index - 6;
+  });
 const ENCODER = new TextEncoder();
 const DECODER = new TextDecoder();
 
 export function toUtf8(bytes: Uint8Array): string {
-    return DECODER.decode(bytes);
+  return DECODER.decode(bytes);
 }
 
 // There are two implementations.
@@ -20,146 +20,146 @@ export function toUtf8(bytes: Uint8Array): string {
 // One optimizes for iteration count, and appends strings.
 // This removes the overhead of TextDecoder.
 export function toHex(bytes: Uint8Array): string {
-    const b = bytes || new Uint8Array();
-    return b.length > 512 ? _toHexLengthPerf(b) : _toHexIterPerf(b);
+  const b = bytes || new Uint8Array();
+  return b.length > 512 ? _toHexLengthPerf(b) : _toHexIterPerf(b);
 }
 function _toHexIterPerf(bytes: Uint8Array): string {
-    let s = "";
-    for (let i = 0; i < bytes.length; ++i) {
-        s += HEX_STRINGS[HEX_CODEPOINTS[HEX_CODES[bytes[i] >> 4]]!];
-        s += HEX_STRINGS[HEX_CODEPOINTS[HEX_CODES[bytes[i] & 15]]!];
-    }
-    return s;
+  let s = "";
+  for (let i = 0; i < bytes.length; ++i) {
+    s += HEX_STRINGS[HEX_CODEPOINTS[HEX_CODES[bytes[i] >> 4]]!];
+    s += HEX_STRINGS[HEX_CODEPOINTS[HEX_CODES[bytes[i] & 15]]!];
+  }
+  return s;
 }
 function _toHexLengthPerf(bytes: Uint8Array): string {
-    const hexBytes = new Uint8Array(bytes.length * 2);
-    for (let i = 0; i < bytes.length; ++i) {
-        hexBytes[i * 2] = HEX_CODES[bytes[i] >> 4];
-        hexBytes[i * 2 + 1] = HEX_CODES[bytes[i] & 15];
-    }
-    return DECODER.decode(hexBytes);
+  const hexBytes = new Uint8Array(bytes.length * 2);
+  for (let i = 0; i < bytes.length; ++i) {
+    hexBytes[i * 2] = HEX_CODES[bytes[i] >> 4];
+    hexBytes[i * 2 + 1] = HEX_CODES[bytes[i] & 15];
+  }
+  return DECODER.decode(hexBytes);
 }
 
 // Mimics Buffer.from(x, 'hex') logic
 // Stops on first non-hex string and returns
 // https://github.com/nodejs/node/blob/v14.18.1/src/string_bytes.cc#L246-L261
 export function fromHex(hexString: string): Uint8Array {
-    const hexBytes = ENCODER.encode(hexString || "");
-    const resultBytes = new Uint8Array(Math.floor(hexBytes.length / 2));
-    let i: number;
-    for (i = 0; i < resultBytes.length; i++) {
-        const a = HEX_CODEPOINTS[hexBytes[i * 2]];
-        const b = HEX_CODEPOINTS[hexBytes[i * 2 + 1]];
-        if (a === undefined || b === undefined) {
-            break;
-        }
-        resultBytes[i] = (a << 4) | b;
+  const hexBytes = ENCODER.encode(hexString || "");
+  const resultBytes = new Uint8Array(Math.floor(hexBytes.length / 2));
+  let i: number;
+  for (i = 0; i < resultBytes.length; i++) {
+    const a = HEX_CODEPOINTS[hexBytes[i * 2]];
+    const b = HEX_CODEPOINTS[hexBytes[i * 2 + 1]];
+    if (a === undefined || b === undefined) {
+      break;
     }
-    return i === resultBytes.length ? resultBytes : resultBytes.slice(0, i);
+    resultBytes[i] = (a << 4) | b;
+  }
+  return i === resultBytes.length ? resultBytes : resultBytes.slice(0, i);
 }
 
 export type CompareResult = -1 | 0 | 1;
 // Same behavior as Buffer.compare()
 export function compare(v1: Uint8Array, v2: Uint8Array): CompareResult {
-    const minLength = Math.min(v1.length, v2.length);
-    for (let i = 0; i < minLength; ++i) {
-        if (v1[i] !== v2[i]) {
-            return v1[i] < v2[i] ? -1 : 1;
-        }
+  const minLength = Math.min(v1.length, v2.length);
+  for (let i = 0; i < minLength; ++i) {
+    if (v1[i] !== v2[i]) {
+      return v1[i] < v2[i] ? -1 : 1;
     }
-    return v1.length === v2.length ? 0 : v1.length > v2.length ? 1 : -1;
+  }
+  return v1.length === v2.length ? 0 : v1.length > v2.length ? 1 : -1;
 }
 
 export type endian = "LE" | "BE" | "le" | "be";
 
 export function writeUInt8(
-    buffer: Uint8Array,
-    offset: number,
-    value: bigint
+  buffer: Uint8Array,
+  offset: number,
+  value: bigint
 ): void {
-    if (offset + 1 > buffer.length) {
-        throw new Error(" Offset is outside the bounds of Uint8Array");
-    }
+  if (offset + 1 > buffer.length) {
+    throw new Error(" Offset is outside the bounds of Uint8Array");
+  }
 
-    value = value & 0xffn;
-    buffer[offset] = Number(value);
+  value = value & 0xffn;
+  buffer[offset] = Number(value);
 }
 
 export function writeUInt16(
-    buffer: Uint8Array,
-    offset: number,
-    value: bigint,
-    littleEndian: endian
+  buffer: Uint8Array,
+  offset: number,
+  value: bigint,
+  littleEndian: endian
 ): void {
-    if (offset + 2 > buffer.length) {
-        throw new Error(" Offset is outside the bounds of Uint8Array");
-    }
-    littleEndian = littleEndian.toUpperCase() as endian;
-    value = value & 0xffffn;
+  if (offset + 2 > buffer.length) {
+    throw new Error(" Offset is outside the bounds of Uint8Array");
+  }
+  littleEndian = littleEndian.toUpperCase() as endian;
+  value = value & 0xffffn;
 
-    if (littleEndian === "LE") {
-        buffer[offset] = Number(value & 0xffn);
-        buffer[offset + 1] = Number((value >> 8n) & 0xffn);
-    } else {
-        buffer[offset] = Number((value >> 8n) & 0xffn);
-        buffer[offset + 1] = Number(value & 0xffn);
-    }
+  if (littleEndian === "LE") {
+    buffer[offset] = Number(value & 0xffn);
+    buffer[offset + 1] = Number((value >> 8n) & 0xffn);
+  } else {
+    buffer[offset] = Number((value >> 8n) & 0xffn);
+    buffer[offset + 1] = Number(value & 0xffn);
+  }
 }
 
 export function writeUInt32(
-    buffer: Uint8Array,
-    offset: number,
-    value: bigint,
-    littleEndian: endian
+  buffer: Uint8Array,
+  offset: number,
+  value: bigint,
+  littleEndian: endian
 ): void {
-    if (offset + 4 > buffer.length) {
-        throw new Error(" Offset is outside the bounds of Uint8Array");
-    }
-    littleEndian = littleEndian.toUpperCase() as endian;
-    value = value & 0xffffffffn;
+  if (offset + 4 > buffer.length) {
+    throw new Error(" Offset is outside the bounds of Uint8Array");
+  }
+  littleEndian = littleEndian.toUpperCase() as endian;
+  value = value & 0xffffffffn;
 
-    if (littleEndian === "LE") {
-        buffer[offset] = Number(value & 0xffn);
-        buffer[offset + 1] = Number((value >> 8n) & 0xffn);
-        buffer[offset + 2] = Number((value >> 16n) & 0xffn);
-        buffer[offset + 3] = Number((value >> 24n) & 0xffn);
-    } else {
-        buffer[offset] = Number((value >> 24n) & 0xffn);
-        buffer[offset + 1] = Number((value >> 16n) & 0xffn);
-        buffer[offset + 2] = Number((value >> 8n) & 0xffn);
-        buffer[offset + 3] = Number(value & 0xffn);
-    }
+  if (littleEndian === "LE") {
+    buffer[offset] = Number(value & 0xffn);
+    buffer[offset + 1] = Number((value >> 8n) & 0xffn);
+    buffer[offset + 2] = Number((value >> 16n) & 0xffn);
+    buffer[offset + 3] = Number((value >> 24n) & 0xffn);
+  } else {
+    buffer[offset] = Number((value >> 24n) & 0xffn);
+    buffer[offset + 1] = Number((value >> 16n) & 0xffn);
+    buffer[offset + 2] = Number((value >> 8n) & 0xffn);
+    buffer[offset + 3] = Number(value & 0xffn);
+  }
 }
 
 export function writeUInt64(
-    buffer: Uint8Array,
-    offset: number,
-    value: bigint,
-    littleEndian: endian
+  buffer: Uint8Array,
+  offset: number,
+  value: bigint,
+  littleEndian: endian
 ): void {
-    if (offset + 8 > buffer.length) {
-        throw new Error(" Offset is outside the bounds of Uint8Array");
-    }
-    littleEndian = littleEndian.toUpperCase() as endian;
-    value = value & 0xffffffffffffffffn;
+  if (offset + 8 > buffer.length) {
+    throw new Error(" Offset is outside the bounds of Uint8Array");
+  }
+  littleEndian = littleEndian.toUpperCase() as endian;
+  value = value & 0xffffffffffffffffn;
 
-    if (littleEndian === "LE") {
-        buffer[offset] = Number(value & 0xffn);
-        buffer[offset + 1] = Number((value >> 8n) & 0xffn);
-        buffer[offset + 2] = Number((value >> 16n) & 0xffn);
-        buffer[offset + 3] = Number((value >> 24n) & 0xffn);
-        buffer[offset + 4] = Number((value >> 32n) & 0xffn);
-        buffer[offset + 5] = Number((value >> 40n) & 0xffn);
-        buffer[offset + 6] = Number((value >> 48n) & 0xffn);
-        buffer[offset + 7] = Number((value >> 56n) & 0xffn);
-    } else {
-        buffer[offset] = Number((value >> 56n) & 0xffn);
-        buffer[offset + 1] = Number((value >> 48n) & 0xffn);
-        buffer[offset + 2] = Number((value >> 40n) & 0xffn);
-        buffer[offset + 3] = Number((value >> 32n) & 0xffn);
-        buffer[offset + 4] = Number((value >> 24n) & 0xffn);
-        buffer[offset + 5] = Number((value >> 16n) & 0xffn);
-        buffer[offset + 6] = Number((value >> 8n) & 0xffn);
-        buffer[offset + 7] = Number(value & 0xffn);
-    }
+  if (littleEndian === "LE") {
+    buffer[offset] = Number(value & 0xffn);
+    buffer[offset + 1] = Number((value >> 8n) & 0xffn);
+    buffer[offset + 2] = Number((value >> 16n) & 0xffn);
+    buffer[offset + 3] = Number((value >> 24n) & 0xffn);
+    buffer[offset + 4] = Number((value >> 32n) & 0xffn);
+    buffer[offset + 5] = Number((value >> 40n) & 0xffn);
+    buffer[offset + 6] = Number((value >> 48n) & 0xffn);
+    buffer[offset + 7] = Number((value >> 56n) & 0xffn);
+  } else {
+    buffer[offset] = Number((value >> 56n) & 0xffn);
+    buffer[offset + 1] = Number((value >> 48n) & 0xffn);
+    buffer[offset + 2] = Number((value >> 40n) & 0xffn);
+    buffer[offset + 3] = Number((value >> 32n) & 0xffn);
+    buffer[offset + 4] = Number((value >> 24n) & 0xffn);
+    buffer[offset + 5] = Number((value >> 16n) & 0xffn);
+    buffer[offset + 6] = Number((value >> 8n) & 0xffn);
+    buffer[offset + 7] = Number(value & 0xffn);
+  }
 }
--- ts_src/index.ts
+++ ts_src/index.ts
@@ -1,82 +1,82 @@
 export function toUtf8(bytes: Uint8Array): string {
-    return Buffer.from(bytes || []).toString();
+  return Buffer.from(bytes || []).toString();
 }
 
 export function toHex(bytes: Uint8Array): string {
-    return Buffer.from(bytes || []).toString("hex");
+  return Buffer.from(bytes || []).toString("hex");
 }
 
 export function fromHex(hexString: string): Uint8Array {
-    return Uint8Array.from(Buffer.from(hexString || "", "hex"));
+  return Uint8Array.from(Buffer.from(hexString || "", "hex"));
 }
 
 export type CompareResult = -1 | 0 | 1;
 export function compare(v1: Uint8Array, v2: Uint8Array): CompareResult {
-    return Buffer.from(v1).compare(Buffer.from(v2)) as CompareResult;
+  return Buffer.from(v1).compare(Buffer.from(v2)) as CompareResult;
 }
 
 export type endian = "LE" | "BE" | "le" | "be";
 
 export function writeUInt8(
-    buffer: Uint8Array,
-    offset: number,
-    value: bigint
+  buffer: Uint8Array,
+  offset: number,
+  value: bigint
 ): void {
-    const buf = Buffer.from(buffer);
-    buf.writeUInt8(Number(value), offset);
-    buffer.set(Uint8Array.from(buf), offset);
+  const buf = Buffer.from(buffer);
+  buf.writeUInt8(Number(value), offset);
+  buffer.set(Uint8Array.from(buf), offset);
 }
 
 export function writeUInt16(
-    buffer: Uint8Array,
-    offset: number,
-    value: bigint,
-    littleEndian: endian
+  buffer: Uint8Array,
+  offset: number,
+  value: bigint,
+  littleEndian: endian
 ): void {
-    littleEndian = littleEndian.toUpperCase() as endian;
+  littleEndian = littleEndian.toUpperCase() as endian;
 
-    const buf = Buffer.alloc(2);
+  const buf = Buffer.alloc(2);
 
-    if (littleEndian === "LE") {
-        buf.writeUInt16LE(Number(value), offset);
-    } else {
-        buf.writeUInt16BE(Number(value), offset);
-    }
-    buffer.set(Uint8Array.from(buf), offset);
+  if (littleEndian === "LE") {
+    buf.writeUInt16LE(Number(value), offset);
+  } else {
+    buf.writeUInt16BE(Number(value), offset);
+  }
+  buffer.set(Uint8Array.from(buf), offset);
 }
 
 export function writeUInt32(
-    buffer: Uint8Array,
-    offset: number,
-    value: bigint,
-    littleEndian: endian
+  buffer: Uint8Array,
+  offset: number,
+  value: bigint,
+  littleEndian: endian
 ): void {
-    littleEndian = littleEndian.toUpperCase() as endian;
+  littleEndian = littleEndian.toUpperCase() as endian;
 
-    const buf = Buffer.alloc(4);
+  const buf = Buffer.alloc(4);
 
-    if (littleEndian === "LE") {
-        buf.writeUInt32LE(Number(value), offset);
-    } else {
-        buf.writeUInt32BE(Number(value), offset);
-    }
-    buffer.set(Uint8Array.from(buf), offset);
+  if (littleEndian === "LE") {
+    buf.writeUInt32LE(Number(value), offset);
+  } else {
+    buf.writeUInt32BE(Number(value), offset);
+  }
+  buffer.set(Uint8Array.from(buf), offset);
 }
 
 export function writeUInt64(
-    buffer: Uint8Array,
-    offset: number,
-    value: bigint,
-    littleEndian: endian
+  buffer: Uint8Array,
+  offset: number,
+  value: bigint,
+  littleEndian: endian
 ): void {
-    littleEndian = littleEndian.toUpperCase() as endian;
+  littleEndian = littleEndian.toUpperCase() as endian;
 
-    const buf = Buffer.alloc(8);
+  const buf = Buffer.alloc(8);
 
-    if (littleEndian === "LE") {
-        buf.writeBigUInt64LE(value, offset);
-    } else {
-        buf.writeBigUInt64BE(value, offset);
-    }
-    buffer.set(Uint8Array.from(buf), offset);
+  if (littleEndian === "LE") {
+    buf.writeBigUInt64LE(value, offset);
+  } else {
+    buf.writeBigUInt64BE(value, offset);
+  }
+  buffer.set(Uint8Array.from(buf), offset);
 }
