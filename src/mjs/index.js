4d53643bd39ad82b1b330ee8de7eb0bd2c371710
junderw <junderwood@bitcoinbank.co.jp>
2024-07-06 22:11:34 +0900

    Add benchmarks and make sure Node is faster

--- src/mjs/index.js
+++ src/mjs/index.js
@@ -1,12 +1,20 @@
-export function toUtf8(bytes) {
-    return Buffer.from(bytes || []).toString();
-}
 export function toHex(bytes) {
     return Buffer.from(bytes || []).toString("hex");
 }
 export function fromHex(hexString) {
     return Uint8Array.from(Buffer.from(hexString || "", "hex"));
 }
+// Same behavior as Buffer.compare()
 export function compare(v1, v2) {
-    return Buffer.from(v1).compare(Buffer.from(v2));
+    const minLength = Math.min(v1.length, v2.length);
+    for (let i = 0; i < minLength; ++i) {
+        if (v1[i] !== v2[i]) {
+            return v1[i] < v2[i] ? -1 : 1;
+        }
+    }
+    return v1.length === v2.length ? 0 : v1.length > v2.length ? 1 : -1;
+}
+const DECODER = new TextDecoder();
+export function toUtf8(bytes) {
+    return DECODER.decode(bytes);
 }
--- ts_src/index.ts
+++ ts_src/index.ts
@@ -1,7 +1,3 @@
-export function toUtf8(bytes: Uint8Array): string {
-  return Buffer.from(bytes || []).toString();
-}
-
 export function toHex(bytes: Uint8Array): string {
   return Buffer.from(bytes || []).toString("hex");
 }
@@ -10,7 +6,21 @@ export function fromHex(hexString: string): Uint8Array {
   return Uint8Array.from(Buffer.from(hexString || "", "hex"));
 }
 
+// API that is copy-paste from browser
+
 export type CompareResult = -1 | 0 | 1;
+// Same behavior as Buffer.compare()
 export function compare(v1: Uint8Array, v2: Uint8Array): CompareResult {
-  return Buffer.from(v1).compare(Buffer.from(v2)) as CompareResult;
+  const minLength = Math.min(v1.length, v2.length);
+  for (let i = 0; i < minLength; ++i) {
+    if (v1[i] !== v2[i]) {
+      return v1[i] < v2[i] ? -1 : 1;
+    }
+  }
+  return v1.length === v2.length ? 0 : v1.length > v2.length ? 1 : -1;
+}
+
+const DECODER = new TextDecoder();
+export function toUtf8(bytes: Uint8Array): string {
+  return DECODER.decode(bytes);
 }
